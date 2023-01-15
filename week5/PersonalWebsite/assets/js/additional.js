60a61a4ff813dbff079f0855df11a66c38658dbd
Adan Cabildo <alcabildo2@student.apc.edu.ph>
2023-01-15 12:01:44 +0800

    Added Time

---
+++ week5/PersonalWebsite/assets/js/additional.js
@@ -0,0 +1,10 @@
+function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var time = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = time;
+}
+setInterval(displayTime, 1000);
+
