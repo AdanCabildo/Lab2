3ae9155b0d1e51739f1b241354299c21f6c9a706
Adan Cabildo <alcabildo2@student.apc.edu.ph>
2023-01-22 11:48:56 +0800

    RESET

--- week5/PersonalWebsite/assets/js/additional.js
+++ week5/PersonalWebsite/assets/js/additional.js
@@ -22,3 +22,24 @@ document.getElementById("currentDate").innerHTML = date;
 function myFunction() {
     alert("stop clicking me (-_-)");
   }
+
+function goBack() {
+    window.history.back()
+  }
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");   
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+        elem.style.left = pos + "px"; 
+      }
+    }
+  }
